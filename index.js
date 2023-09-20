8c722bfcd258dcddea4cdf79e48cfb44865625f8
alexandernyaga <alexandernyaga1999@gmail.com>
2023-09-20 17:25:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function countDown(startingNumber) {
+    if (typeof startingNumber === 'number' && startingNumber > 0 && Number.isInteger(startingNumber)) {
+      for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+      }
+    } else {
+      console.log("Please provide a positive integer as input.");
+    }
+  }
+  
+  countDown(10);
+
