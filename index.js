9f0289f920fee577d0061b007bf7eaf08075bbe0
alexander <alexandernyaga1999@gmail.com>
2023-09-30 17:18:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function countDown(startingNumber) {
   
   countDown(10);
 
+  function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  
+
